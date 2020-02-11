101beb221328e37edbc26c73f98fa2d9c1e73332
KenKi9 <1595953919@qq.com>
2020-02-11 21:35:24 +0800

    time

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,8 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    date: '',
-    time: '',
+   
+  },
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
   },
 
   
