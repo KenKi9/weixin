5b45c157129b633ab166584b8af8366379872cb3
KenKi9 <1595953919@qq.com>
2020-02-12 11:22:11 +0800

    time

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,14 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-   
+    year: "2000",
+    mouth: "00",
+    arr: [],
+    mytimes: 0
   },
-  bindDateChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+  chagtime: function (event) {
+    var value = event.detail.value;
     this.setData({
-      date: e.detail.value
-    })
+      year: value.split("-")[0],
+      mouth: value.split("-")[1]
+    });
+    this.settime();
   },
+  clear: function () {
+    this.setData({
+      year: "",
+      mouth: "",
+      arr: [],
+      mytimes: 0
+    });
+  },
+  onShow: function () {
+    this.settime();
+  },
+  settime: function () {
+    console.log;
+    var _a = this.data, year = _a.year, mouth = _a.mouth;
+    var oldtime = mouth === "00" ? new Date(year).getTime() : new Date(year + "-" + mouth).getTime();
+    var newtime = new Date(new Date().getFullYear() + "-" + (new Date().getMonth() + 1)).getTime();
+    if (newtime > oldtime) {
+      var arr = newtime - oldtime;
+      var sums = Math.ceil(arr / (1000 * 60 * 60 * 24 * 31));
+      this.setData({
+        mytimes: sums
+      });
+    }
+  },
+  
 
   
 
